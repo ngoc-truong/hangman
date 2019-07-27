cc6b6b1a3ee256e5f5cb4b556fbc1737703c04aa
Ngoc Truong <ngoc.truong@posteo.de>
2019-07-27 18:36:41 +0200

    Fixed wrong trys counter and add saving/loading functionality

--- hangman.rb
+++ hangman.rb
@@ -1,14 +1,16 @@
+require 'json'
+
 
 class Game
-    attr_accessor :word, :past_guesses, :num_guesses, :max_guesses, :player
+    attr_accessor :word, :past_guesses, :wrong_guesses, :max_guesses, :player
 
-    def initialize(max_guesses, dictionary)
-        @words = dictionary
-        @word = ""
-        @past_guesses = []
-        @wrong_guesses = 0
+    def initialize(max_guesses, dictionary, word = "", past_guesses = [], wrong_guesses = 0, player = Player.new )
         @max_guesses = max_guesses
-        @player = Player.new
+        @words = dictionary
+        @word = word
+        @past_guesses = past_guesses
+        @wrong_guesses = wrong_guesses
+        @player = player
     end
 
     ### Methods for preprocessing and getting the target word
@@ -58,7 +60,10 @@ class Game
     end
 
     def start
-        self.set_word
+        # Only if there is no word from a saved game
+        if self.word == ""  
+            self.set_word
+        end
 
         while self.has_underscores?
             if @past_guesses.empty? 
@@ -66,47 +71,109 @@ class Game
             end
 
             guess = @player.guess
+
+            # Save the game
+            while guess == "save"
+                print "Please provide a filename: "
+                filename = gets.chomp.downcase
+
+                self.save_as(filename)
+                guess = @player.guess
+            end
+
+            # Chosen char already in past_guesses
+            while @past_guesses.include?(guess)
+                puts ""
+                puts ""
+                puts ""
+                puts "You already took this character. Please choose another one!!!".upcase
+                puts ""
+                puts ""
+                puts ""
+                guess = @player.guess
+            end
+
             @past_guesses << guess
+
             puts self.print_result
             puts ""
             puts "Your past guesses are: #{@past_guesses.join(', ')}"
 
+            # Update wrong_guesses
             if !self.word.chars.include?(guess)
                 @wrong_guesses += 1
             end
         
-            puts ""
             puts "You have #{@wrong_guesses} wrong guesses. You have #{@max_guesses - @wrong_guesses} wrong guesses left." 
             puts ""
             if @wrong_guesses >= @max_guesses 
-                puts ""
+                puts "----------------"
                 puts "Sorry, you lose!"
+                puts "----------------"
                 puts "The word was #{self.word}."
                 puts ""
                 return
             end
         end
 
-        puts ""
+        puts "-------------------------"
         puts "Congratulations, you won!"
-        puts ""
+        puts "-------------------------"
+    end
+
+    ### Saving and JSON serialization
+    def save_as(filename)
+        file = File.open("./save_games/#{filename}.txt", "w")
+        file.puts self.to_json
+        file.close
+    end
+
+    def load(file)
+        Game.from_json(file)
+    end
+
+    def to_json
+        JSON.dump( {
+            :max_guesses => @max_guesses,
+            :dictionary => @words,
+            :word => @word, 
+            :past_guesses => @past_guesses,
+            :wrong_guesses => @wrong_guesses
+        })
+    end
+
+    def self.from_json(file)
+        data = JSON.load(File.read(file))
+        self.new(   data["max_guesses"], 
+                    data["dictionary"], 
+                    data["word"],
+                    data["past_guesses"], 
+                    data["wrong_guesses"], 
+                    Player.new )    # Json only saves Player object as a string, so we create a new one
     end
 end
 
 
 class Player
     def guess
-        puts ""
-        puts "Please provide me a character, beep beep di boop!"
+        puts "---------------------------------------------------------------------------"
+        puts "You can save your game by typing 'save'."
+        puts "If you want to continue, provide me a character, please, beep beep di boop!"
+        puts "---------------------------------------------------------------------------"
+
         char = gets.chomp
         puts ""
 
+        if char.downcase == "save"
+            return char
+        end
+
         while char.size > 1
-            puts ""
-            puts "I am sorry, but you cannot choose that many characters, dum dum!"
+            puts "---------------------------------------------------------------"
+            puts "I am sorry, but you cannot choose that many characters, dumdum!"
             puts "Please choose another character!"
+            puts "---------------------------------------------------------------"
             char = gets.chomp
-            puts ""
         end
         
         return char
@@ -115,5 +182,22 @@ end
 
 
 # Testing
-game = Game.new(10, "5desk.txt")
+puts "----------------------------------------"
+puts "Do you want to load your old game (y/n)?"
+puts "----------------------------------------"
+
+answer = gets.chomp.downcase
+
+if answer == "y" || answer == "load"
+    puts Dir.entries("./save_games/")
+    puts "------------------------------"
+    puts "Which one do you want to load?"
+    puts "------------------------------"
+
+    filename = gets.chomp
+    game = Game.from_json("./save_games/#{filename}")
+else 
+    game = Game.new(10, "5desk.txt")
+end
+
 game.start
